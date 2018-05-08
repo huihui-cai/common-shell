edb33208d2b733b19ac1b5ee609421376ebdb218
liangxh <liangxh@seagroup.com>
2018-05-08 19:52:25 +0800

    update rm direct

--- cmd/rm.sh
+++ cmd/rm.sh
@@ -5,10 +5,21 @@ TrashDir="/Users/liangxh/.Trash"
 for((i=1; $i <= $#; i++));
 do
 	rmFile=$(eval echo \$$i)
+	if [[ $rmFile == "-"* ]]; then
+		continue
+	fi
 	realFile=`sh ~/.Tool/readlink.sh $rmFile`
 	if [[ $realFile == $TrashDir* ]]; then
-		rm $realFile
+		rm -rf $realFile
 	else
+		trashFileName=$TrashDir/`basename $realFile`
+		if [ -e $trashFileName ]; then
+			fileWithSuffix=$trashFileName"."`date +%s`
+			if [ -e $fileWithSuffix ]; then
+				rm -rf $fileWithSuffix
+			fi
+			mv $trashFileName $fileWithSuffix
+		fi
 		mv $realFile $TrashDir
 	fi
 done
